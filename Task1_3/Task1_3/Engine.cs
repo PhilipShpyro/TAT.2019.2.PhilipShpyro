c2e49a6d5e916ffdb96a2b2f5d8006de1a15209d
Philip Shpyro <philip.shpyro@gmail.com>
2020-02-23 23:51:24 +0300

    Task1_3 added

---
+++ Task1_3/Task1_3/Engine.cs
@@ -0,0 +1,21 @@
+﻿namespace Task1_3
+{
+    class Engine
+    {
+        private int power;
+        private double volume;
+        private string type;
+        private string serialNumber;
+        public Engine(int power, double volume, string type, string serialNumber)
+        {
+            this.power = power;
+            this.volume = volume;
+            this.type = type;
+            this.serialNumber = serialNumber;
+        }
+        public override string ToString()
+        {
+            return "    engine: volume " + volume + "l, type " + type + ", power " + power + " bhp, serial number " + serialNumber;
+        }
+    }
+}
---
+++ Task1_3/Task1_3/Vehicle.cs
@@ -0,0 +1,21 @@
+﻿namespace Task1_3
+{
+    abstract class Vehicle
+    {
+        private Engine engine;
+        private Transmission transmission;
+        private Chassis chassis;
+
+        public Vehicle(Engine engine, Transmission transmission, Chassis chassis)
+        {
+            this.engine = engine;
+            this.transmission = transmission;
+            this.chassis = chassis;
+        }
+
+        public override string ToString()
+        {
+            return engine + "\n" + transmission + "\n" + chassis;
+        }
+    }
+}
