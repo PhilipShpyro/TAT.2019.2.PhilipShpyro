7286deab5b984979551719682b5e28c8aa9d160e
Philip Shpyro <philip.shpyro@gmail.com>
2020-02-27 01:45:07 +0300

    Task2_3 added

---
+++ Task2_3/Task2_3/Program.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace Task2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver browser = new ChromeDriver();
+            WebDriverWait wait = new WebDriverWait(browser, new TimeSpan(0, 0, 30));
+
+            MailRuPage mailRu = new MailRuPage(browser, wait);
+            mailRu.Authorize("vpupchick", "AutomatedVasya247");
+
+            
+
+            MailMessageWriter messageWriter = new MailMessageWriter(browser, wait);
+            messageWriter.Write("a");
+        }
+    }
+}
